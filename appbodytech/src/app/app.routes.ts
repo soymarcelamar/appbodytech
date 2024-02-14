ef0764d08151e965a9d8f26a6538b899111006e9
Marcela Martínez <63815530+soymarcelamar@users.noreply.github.com>
2024-02-14 15:56:31 -0500

    #1 Add home component
    Create home component

---
+++ appbodytech/src/app/app.routes.ts
@@ -0,0 +1,17 @@
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { NgModule } from '@angular/core';
+
+
+//Crear rutas
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: 'home', component: HomeComponent },
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+      
+  export class AppRoutingModule { }
